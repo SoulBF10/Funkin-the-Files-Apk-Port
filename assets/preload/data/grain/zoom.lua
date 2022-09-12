8ae3bc00e32def7a8350c79146f8b38254a5fc2c
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2022-09-12 10:41:59 -0300

    Add files via upload

---
+++ assets/preload/data/grain/zoom.lua
@@ -0,0 +1,21 @@
+function onBeatHit()
+if curBeat == 248 then
+      setProperty('defaultCamZoom', 0.45);
+   end
+if curBeat == 252 then
+      setProperty('defaultCamZoom', 0.5);
+   end
+if curBeat == 253 then
+      setProperty('defaultCamZoom', 0.55);
+   end
+if curBeat == 254 then
+      setProperty('defaultCamZoom', 0.6);
+   end
+if curBeat == 255 then
+      setProperty('defaultCamZoom', 0.65);
+   end
+if curBeat == 256 then
+      setProperty('defaultCamZoom', 0.7);
+   end
+end
+ 
