1da27d874f39cbb2565a180c1f71b20fbf1d9eeb
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2022-09-12 10:50:01 -0300

    Add files via upload

---
+++ assets/preload/data/lies/script.lua
@@ -0,0 +1,5 @@
+function onBeatHit()
+   if curBeat == 247 then
+      setProperty('defaultCamZoom', 0.9);
+   end
+end
---
+++ assets/preload/data/lies/zoom.lua
@@ -0,0 +1,111 @@
+function onBeatHit()
+if curBeat == 42 then
+      setProperty('defaultCamZoom', 1.2);
+   end
+if curBeat == 48 then
+      setProperty('defaultCamZoom', 0.95);
+   end
+if curBeat == 74 then
+      setProperty('defaultCamZoom', 1.2);
+   end
+if curBeat == 80 then
+      setProperty('defaultCamZoom', 1.0);
+   end
+if curBeat == 84 then
+      setProperty('defaultCamZoom', 1.05);
+   end
+if curBeat == 88 then
+      setProperty('defaultCamZoom', 1.1);
+   end
+if curBeat == 92 then
+      setProperty('defaultCamZoom', 1.15);
+   end
+if curBeat == 96 then
+      setProperty('defaultCamZoom', 1.2);
+   end
+if curBeat == 100 then
+      setProperty('defaultCamZoom', 1.25);
+   end
+if curBeat == 104 then
+      setProperty('defaultCamZoom', 1.3);
+   end
+if curBeat == 108 then
+      setProperty('defaultCamZoom', 1.35);
+   end
+if curBeat == 112 then
+      setProperty('defaultCamZoom', 1.0);
+   end
+if curBeat == 116 then
+      setProperty('defaultCamZoom', 1.05);
+   end
+if curBeat == 120 then
+      setProperty('defaultCamZoom', 1.1);
+   end
+if curBeat == 124 then
+      setProperty('defaultCamZoom', 1.15);
+   end
+if curBeat == 128 then
+      setProperty('defaultCamZoom', 1.2);
+   end
+if curBeat == 132 then
+      setProperty('defaultCamZoom', 1.25);
+   end
+if curBeat == 136 then
+      setProperty('defaultCamZoom', 1.3);
+   end
+if curBeat == 140 then
+      setProperty('defaultCamZoom', 1.35);
+ end
+if curBeat == 144 then
+      setProperty('defaultCamZoom', 1.0);
+ end
+if curBeat == 153 then
+      setProperty('defaultCamZoom', 1.1);
+   end
+if curBeat == 176 then
+      setProperty('defaultCamZoom', 1.05);
+   end
+if curBeat == 247 then
+      setProperty('defaultCamZoom', 0.9);
+   end
+if curBeat == 264 then
+      setProperty('defaultCamZoom', 1.2);
+   end
+if curBeat == 280 then
+      setProperty('defaultCamZoom', 0.9);
+   end
+if curBeat == 296 then
+      setProperty('defaultCamZoom', 1.2);
+   end
+if curBeat == 312 then
+      setProperty('defaultCamZoom', 0.9);
+   end
+if curBeat == 328 then
+      setProperty('defaultCamZoom', 1.2);
+   end
+if curBeat == 344 then
+      setProperty('defaultCamZoom', 0.9);
+   end
+if curBeat == 360 then
+      setProperty('defaultCamZoom', 1.2);
+   end
+if curBeat == 372 then
+      setProperty('defaultCamZoom', 1.3);
+   end
+if curBeat == 373 then
+      setProperty('defaultCamZoom', 1.4);
+   end
+if curBeat == 374 then
+      setProperty('defaultCamZoom', 1.5);
+   end
+if curBeat == 375 then
+      setProperty('defaultCamZoom', 1.6);
+   end
+if curBeat == 376 then
+      setProperty('defaultCamZoom', 1.7);
+   end
+if curBeat == 378 then
+      setProperty('defaultCamZoom', 1.0);
+   end
+end
+   
